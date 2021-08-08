8ee4e51048c4b106edd8500fe6a7adab3333f2f6
Akmal <akmal@go.alif.academy>
2021-08-08 19:00:48 +0500

    new

--- test_scrape.py
+++ test_scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import gspread
 import datetime
-
+iphone12 = "https://alif.shop/telefony/iphone-12/"
 
 def product_link(link):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'}
@@ -25,18 +25,22 @@ def output(product, partner_name):
     sh = gc.open('test').sheet1
     sh.append_row([str(product['date']), str(product['name']), str(product['price']), partner_name])
     return
-# Icom - iPhone 11 64 Gb
-data = product_link("https://alif.shop/phone/iphone/iphone-11-64-gb-ru-7/")
-product = parse(data)
-output(product,"iCom")
+# Connect - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-ru/")),"iCom")
+                           https://alif.shop/telefony/iphone-12/iphone-12-pro-max-256-gb-nano-simesim-ru-ru/
+# Sam - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("iphone12""iphone-12-pro-max-256-gb-nano-simesim-ru-2/")),"iCom")
 
-# Icom - IPHONE 12 PRO MAX 256 GB (NANO-SIM+ESIM)
-data = product_link("https://alif.shop/phone/iphone/iphone-12-pro-max-256-gb-nano-simesim-ru-7/")
-product = parse(data)
-output(product,"iCom")
-
+# Dumarket - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-3/")),"iCom")
+# Mobisell - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru/")),"iCom")
+# Mobi Like - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-5/")),"iCom")
+# TajStore - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-ru-2/")),"iCom")
+# Real 2 - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-6-ru/")),"iCom")
 # Icom - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-data = product_link("https://alif.shop/telefony/iphone-12/iphone-12-pro-max-128-gb-nano-simnano-sim-ru-4/")
-product = parse(data)
-output(product,"iCom")
-
+output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-7/")),"iCom")
+                           https://alif.shop/telefony/iphone-12/iphone-12-pro-max-256-gb-nano-simesim-ru-7/
