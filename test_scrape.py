17d7349bee0b0888b3842bd746fb66f0731d205f
Akmal <akmal@go.alif.academy>
2021-08-10 18:00:34 +0500

    last commits

--- test_scrape.py
+++ test_scrape.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import gspread
 import datetime
+import pandas as pd
+
+df = pd.read_csv("dict.csv")
+new_df = df.to_dict()
+
 iphone12 = "https://alif.shop/telefony/iphone-12/"
 
 def product_link(link):
@@ -25,22 +30,34 @@ def output(product, partner_name):
     sh = gc.open('test').sheet1
     sh.append_row([str(product['date']), str(product['name']), str(product['price']), partner_name])
     return
-# Connect - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-ru/")),"iCom")
-                           https://alif.shop/telefony/iphone-12/iphone-12-pro-max-256-gb-nano-simesim-ru-ru/
-# Sam - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("iphone12""iphone-12-pro-max-256-gb-nano-simesim-ru-2/")),"iCom")
 
-# Dumarket - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-3/")),"iCom")
-# Mobisell - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru/")),"iCom")
-# Mobi Like - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-5/")),"iCom")
-# TajStore - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-ru-2/")),"iCom")
-# Real 2 - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-6-ru/")),"iCom")
-# Icom - IPHONE 12 PRO MAX 128 GB (NANO-SIM+NANO-SIM)
-output(parse(product_link("https://alif.shop/telefony/iphone-12-pro-max-256-gb-nano-simesim-ru-7/")),"iCom")
-                           https://alif.shop/telefony/iphone-12/iphone-12-pro-max-256-gb-nano-simesim-ru-7/
+
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-ru/")),"Connect")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-2/")),"Sam")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-3/")),"Dumarket")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru/")),"Mobisell")  
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-5/")),"Mobi Like")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-ru-2/")),"TajStore")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-6-ru/")),"Real 2")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simnano-sim-ru/")),"New Mobile") 
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-ru/")),"Galaxy Dushanbe")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-ru-2/")),"City Mobile")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru/")),"Ali Store")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4/")),"Rudaki Mobile")  
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-6/")),"Rudaki Mobile (Dom Pechati)")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-7/")),"iCom")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-ru-2/")),"Salam store")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-ru-3/")),"Boir's Mobile Sadbarg")    
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-3-ru/")),"Kalla Mobile")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-ru-ru/")),"Svyaznoy")  
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru-ru-ru-ru/")),"reStore.tj")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru-ru-ru/")),"Ok Mobile")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru-ru-2/")),"Nasha set")
+# output(parse(product_link(iphone12+"iphone-12-pro-max-256-gb-nano-simesim-ru-4-ru-ru/")),"Game Shop")                        
+
+
+partner_dict = {"iphone-12-pro-max-256-gb-nano-simesim-ru-ru/":"Connect","iphone-12-pro-max-256-gb-nano-simesim-ru-2/":"Sam"}
+# print(partner_dict)
+
+for key in partner_dict:
+     output(parse(product_link(iphone12+key)),partner_dict[key])
